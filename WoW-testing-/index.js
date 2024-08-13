318301164e4fa713f3494a4ca6202681863d400b
Cédric <cedbossant@gmail.com>
2024-08-13 09:51:49 +0200

    suite projet

---
+++ WoW-testing-/index.js
@@ -0,0 +1,10 @@
+const generateA = document.getElementById("generateA");
+const generateH = document.getElementById("generateH");
+
+let AArace = ["Humain", "EDN", "Nain"];
+let AAclass = ["Guerrier", "mage", "Voleur", "Pretre", "Démoniste"];
+
+let AHrace = ["Orc", "Mort-Vivant", "Troll"];
+let AHclass = ["Guerrier", "mage", "Voleur", "Pretre", "Démoniste"];
+
+const generateAlly = () => {};
